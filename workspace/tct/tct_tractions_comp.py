fa9f7fc442539eb4420f1e2ae783525039cbb412
Andino Boerst <andino.boerst@gmail.com>
2025-03-06 16:21:53 +0000

    reverse last commit

--- workspace/tct/tct_tractions_comp.py
+++ workspace/tct/tct_tractions_comp.py
@@ -20,6 +20,8 @@ class TCTApplyFixedTractions(get_TCT_class(DEFORMATION)):
     def _preprocess(self) -> None:
         super()._preprocess()
 
+        self.bottom_half_nodes = self.get_nodes(self.bottom_half)
+
         self.neumann_interface_marker = 88
 
         self.add_neumann_bc(self.interface_boundary, self.neumann_interface_marker)
